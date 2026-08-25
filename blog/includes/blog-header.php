4e123a895b4a6fbb59950fc493966dde2685c654
maxnegiba <154560705+maxnegiba@users.noreply.github.com>
2026-08-25 10:05:59 +0300

    Align blog header with MB Bau branding

--- blog/includes/blog-header.php
+++ blog/includes/blog-header.php
@@ -1,8 +1,4 @@
 <?php
-// Setări pentru blog
-$page_title = $page_title ?? 'Blog | ' . 'Dachdecker Berlin Brandenburg | Der Hausmeister Michael GmbH';
-$page_description = $page_description ?? 'Neueste Nachrichten, Tipps und Projekte aus der Welt der Dachdecker, Klempner und Zimmerleute.';
-
-// Include header-ul principal al site-ului
+$page_title = $page_title ?? 'Blog | MB Bau Dienstleistungen Berlin';
+$page_description = $page_description ?? 'Tipps, Einblicke und Informationen rund um Dacharbeiten, Fassade und Bauprojekte von MB Bau Dienstleistungen.';
 include(__DIR__ . '/../../includes/header.php');
-?>
