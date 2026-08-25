6dca1dd9e74811ba6b643f5bd4e2f05dbf1fcce2
maxnegiba <154560705+maxnegiba@users.noreply.github.com>
2026-08-25 09:07:45 +0300

    Update floating contact buttons phone

--- includes/floating-buttons.php
+++ includes/floating-buttons.php
@@ -1,19 +1,15 @@
-<?php
-$base_url = 'http://' . $_SERVER['HTTP_HOST'] . '/site-acoperisuri/';
-?>
 <div class="floating-buttons">
     <div class="floating-main">
         <i class="icon-phone"></i>
     </div>
     <div class="floating-sub-buttons">
-        <a href="tel:+491626781242" class="floating-btn call" title="Anrufen">
+        <a href="tel:+4917614122627" class="floating-btn call" title="Anrufen">
             <i class="icon-call"></i>
         </a>
-        <a href="https://wa.me/491626781242" class="floating-btn whatsapp" title="WhatsApp">
+        <a href="https://wa.me/4917614122627" class="floating-btn whatsapp" title="WhatsApp">
             <i class="icon-whatsapp"></i>
         </a>
-        <a href="mailto:info@hausmeistermichael-gmbh.de
-" class="floating-btn email" title="E-Mail">
+        <a href="mailto:info@hausmeistermichael-gmbh.de" class="floating-btn email" title="E-Mail">
             <i class="icon-email"></i>
         </a>
     </div>
